7385c5a8ddb4f108065bd3af61b0a37dd7cd1020
jugyo <jugyo.org@gmail.com>
2012-03-13 07:42:51 +0900

    rss はキャッシュしない

--- app.rb
+++ app.rb
@@ -7,19 +7,6 @@ CACHE = ActiveSupport::Cache.lookup_store(:dalli_store)
 CACHE_EXPIRES_IN = ENV['CACHE_EXPIRES_IN'].to_i rescue 60
 DEBUG = ENV['DEBUG']
 
-# TODO: refactor
-def get_content(url)
-  if result = CACHE.read(url)
-    puts "read cache for: #{url}" if DEBUG
-    result
-  else
-    puts "write cache for: #{url}" if DEBUG
-    result = open(url).read
-    CACHE.write(url, result, :expires_in => CACHE_EXPIRES_IN)
-    result
-  end
-end
-
 def text(url)
   if result = CACHE.read(url)
     puts "read cache for: #{url}" if DEBUG
@@ -38,9 +25,7 @@ end
 
 get '/rss' do
   content_type 'application/xml;charset=UTF-8'
-  url = params[:url]
-  content = get_content(url)
-  rss = Nokogiri::XML(content)
+  rss = Nokogiri::XML(open(params[:url]).read)
   rss.xpath('//item').each do |item|
     description = Nokogiri::XML::Node.new 'description', rss
     link = item.xpath('link').first.content
